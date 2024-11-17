d94a6b90e8a3ee9a04703a492340f1051a201237
Nicolas Noir <136653685+Nicolas-Noir@users.noreply.github.com>
2024-11-17 20:10:06 -0300

    Add files via upload
    Se terminó la parte de los dados de forma correcta.

--- TP2/TP2.py
+++ TP2/TP2.py
@@ -16,34 +16,73 @@ img_hls = cv2.cvtColor(monedas_blur, cv2.COLOR_BGR2HLS)
 
 h, l, s = cv2.split(img_hls)
 
-l_smooth = cv2.blur(l, (15, 15))
-s_smooth = cv2.blur(s, (15, 15))
-h_smooth = cv2.blur(h, (15, 15))
+s_smooth = cv2.blur(s, (7, 7))
 
-img_smooth_l = cv2.cvtColor(cv2.merge((h, l_smooth, s)), cv2.COLOR_HLS2RGB)
-
-retval, bin_s = cv2.threshold(s_smooth, 14, 255, cv2.THRESH_BINARY)
-
+_ , bin_s = cv2.threshold(s_smooth, 14, 255, cv2.THRESH_BINARY)
 
 B = cv2.getStructuringElement(cv2.MORPH_RECT, (40,40))
 Aclau = cv2.morphologyEx(bin_s, cv2.MORPH_CLOSE, B)
 
-B = cv2.getStructuringElement(cv2.MORPH_RECT, (50,50))
+B = cv2.getStructuringElement(cv2.MORPH_RECT, (35,35))
 Aop = cv2.morphologyEx(Aclau, cv2.MORPH_OPEN, B)
 
-contours, hierarchy = cv2.findContours(Aop, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+contours, _ = cv2.findContours(Aop, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+mask = np.zeros_like(Aop, dtype=np.uint8)
 
 contornos_circulo_areas  = []
 contornos_solos = []
-
 
 for contour in contours:
     a = cv2.contourArea(contour)
     p_c = cv2.arcLength(contour, True)**2
     fdf = a/p_c
     if fdf > 0.062:
-        contornos_circulo_areas .append((contour, a))
+        contornos_circulo_areas.append((contour, a))
         contornos_solos.append(contour)
+    else:
+        cv2.drawContours(mask, [contour], -1, 255, thickness=cv2.FILLED)
+
+
+B = cv2.getStructuringElement(cv2.MORPH_RECT, (150, 150))
+dado_procesado = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, B)
+
+B = cv2.getStructuringElement(cv2.MORPH_RECT, (35,35))
+dados_redondos = cv2.morphologyEx(dado_procesado, cv2.MORPH_OPEN, B)
+
+contornos_dados_areas, _ = cv2.findContours(dados_redondos, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+imagen_original = monedas.copy()
+
+lista_cantidad_dados_puntos = []
+
+for dado in contornos_dados_areas:
+    x, y, w, h = cv2.boundingRect(dado)
+
+    dado_recortado = imagen_original[y:y+h, x:x+w]
+    dado_gris = cv2.cvtColor(dado_recortado, cv2.COLOR_BGR2GRAY)
+
+    dado_blur = cv2.GaussianBlur(dado_gris, (9, 9), 2)
+    _, binii = cv2.threshold(dado_blur, 170,255, cv2.THRESH_BINARY)
+
+    imagen_invertida = cv2.bitwise_not(binii)
+
+    circulos_dados, _ = cv2.findContours(imagen_invertida, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    puntos_dado = 0
+    for contour in circulos_dados:
+        a = cv2.contourArea(contour)
+        p_c = cv2.arcLength(contour, True)**2
+        fdf = a/p_c
+        if fdf > 0.062:
+            puntos_dado+=1 
+
+    lista_cantidad_dados_puntos.append(puntos_dado)
+
+print(lista_cantidad_dados_puntos)
+
+#--monedas--
+
 
 moneditas = cv2.cvtColor(monedas_copy, cv2.COLOR_BGR2RGB)
 
@@ -80,6 +119,8 @@ if min_area / max_area < 0.85:
         monedas_10_peso = monedas_10_50
     elif monedas_10_50 == 0:
         monedas_10_50 = monedas_10_peso
+    
+    print(monedas_50, monedas_peso, monedas_10_peso)
 
 else:
     monedas_10_peso = 0
@@ -130,56 +171,6 @@ else:
             else:
                 monedas_peso += 1
 
-print(monedas_50, monedas_peso, monedas_10_peso)
+    print(monedas_50, monedas_peso, monedas_10_peso)
 
-
-
-
-
-#--------------------------------------------------
-
-
-
-f = monedas.copy()
-esc_grises = cv2.cvtColor(f,cv2.COLOR_BGR2GRAY)
-
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(5,5))
-f_mg = cv2.morphologyEx(esc_grises, cv2.MORPH_GRADIENT, kernel,iterations=2)
-
-elpepe2=cv2.subtract(esc_grises, f_mg)
-_, binii = cv2.threshold(elpepe2, 170,255, cv2.THRESH_BINARY)
-
-B = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
-pingo = cv2.morphologyEx(elpepe2, cv2.MORPH_CLOSE, B,iterations=15)
-
-_, puntos_dados = cv2.threshold(pingo, 20,255, cv2.THRESH_BINARY)
-
-imagen_invertida = cv2.bitwise_not(puntos_dados)
-
-_, _, _, centroids = cv2.connectedComponentsWithStats(imagen_invertida, 8, cv2.CV_32S)
-
-B = cv2.getStructuringElement(cv2.MORPH_RECT, (40,40))
-Aclau = cv2.morphologyEx(puntos_dados, cv2.MORPH_OPEN, B, iterations=10)
-
-B = cv2.getStructuringElement(cv2.MORPH_RECT, (40,40))
-Aclau2 = cv2.morphologyEx(Aclau, cv2.MORPH_OPEN, B, iterations=10)
-
-contornos_dados, _ = cv2.findContours(Aclau2, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-
-dicc_dados = {}
-contador_dados = 0
-
-for dados in contornos_dados[:-1]:
-    puntos_dentro = []
-    x, y, w, h = cv2.boundingRect(dados)
-    for punto in centroids:
-        if x <= punto[0] <= x + w and y <= punto[1] <= y + h:
-            puntos_dentro.append(punto)
-    if puntos_dentro:
-        dicc_dados[contador_dados] = puntos_dentro
-
-    contador_dados+=1
-
-for cantidad in range(len(dicc_dados)):
-    print(f" dado numero {cantidad+1}, con {len(dicc_dados[cantidad])}")
 
